e8c3f9c818b640dbf898ff40c6319fc1f0c570b0
majdhorani <majd1999majd@gmail.com>
2021-10-04 11:43:24 +0300

    Dashboard Header Widget Created - Dashboard Title Widget Created

---
+++ lib/widgets/dashboard/dashboard_header.dart
@@ -0,0 +1,65 @@
+import 'package:admin/dialogs/settings_dialog.dart';
+import 'package:admin/providers/MenuProvider.dart';
+import 'package:admin/responsive.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../constants.dart';
+
+class DashboardHeader extends StatelessWidget {
+  const DashboardHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 32),
+              child: !Responsive.isMobile(context)
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Wrap(
+                            children: [
+                              Text("Device Code: 9a8273d32",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 10,
+                                  )),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                child: Text("|",
+                                    style: TextStyle(
+                                      color: textGray,
+                                      fontSize: 10,
+                                    )),
+                              ),
+                              Text("Device Code: 9a8273d32",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 10,
+                                  )),
+                            ],
+                          ),
+                        ),
+                        IconButton(
+                            padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
+                            color: Colors.black,
+                            icon: Icon(Icons.settings),
+                            onPressed: () {
+                              SettingsDialog(context: context);
+                            },
+                            constraints: BoxConstraints()),
+                      ],
+                    )
+                  : null),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/dashboard/dashboard_title.dart
@@ -0,0 +1,74 @@
+import 'package:admin/dialogs/settings_dialog.dart';
+import 'package:admin/providers/MenuProvider.dart';
+import 'package:admin/responsive.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../constants.dart';
+
+class DashboardTitle extends StatelessWidget {
+  final String title;
+  final String descriptions;
+  final bool showAddButton;
+  const DashboardTitle({required this.title, required this.descriptions, required this.showAddButton});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        // context.read<MenuProvider>().controlMenu
+                        Text(
+                          title,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5
+                              ?.copyWith(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    Text(descriptions,
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: textGray,
+                            fontWeight: FontWeight.w600)),
+                  ],
+                ),
+              ),
+              if (showAddButton)
+                ElevatedButton.icon(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    backgroundColor: primaryColor,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: defaultPadding,
+                      vertical: defaultPadding /
+                          (Responsive.isMobile(context) ? 1 : 1),
+                    ),
+                  ),
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  label: Text("Add New"),
+                ),
+            ],
+          ),
+          SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
