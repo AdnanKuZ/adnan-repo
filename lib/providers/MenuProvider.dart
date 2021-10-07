394d5bd6d1d5ffd1caf110af949db373df567d83
Joseph Kalash <jkalash4@gmail.com>
2021-10-07 12:37:52 +0300

    fix2

--- lib/providers/MenuProvider.dart
+++ lib/providers/MenuProvider.dart
@@ -6,11 +6,7 @@ import 'package:flutter/material.dart';
 class MenuProvider extends ChangeNotifier {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   GlobalKey<ScaffoldState> get scaffoldKey => _scaffoldKey;
-<<<<<<< HEAD
   DashboardRoute dashboardRoute = DashboardRoute.ContactUs;
-=======
-  DashboardRoute dashboardRoute = DashboardRoute.Policies;
->>>>>>> 3509764ae2db05ed7ef05d1a88c8ec54ded9080c
 
   void controlMenu() {
     if (!_scaffoldKey.currentState!.isDrawerOpen) {
